3806b9954321e5b701040d0bbb3b3fe1e75bed78
DashDeipayan <deipayan.dash8@gmail.com>
2022-01-27 04:11:26 +0530

    added cookie

--- script.js
+++ script.js
@@ -4,17 +4,30 @@ mixpanel.track("Page opened", {
 	source: "Venus Fly Trap",
 });
 
+let getCookies = document.cookie
+	.split(";")
+	.map((cookie) => cookie.split("="))
+	.reduce(
+		(acc, [key, value]) => ({
+			...acc,
+			[key.trim()]: decodeURIComponent(value),
+		}),
+		{}
+	);
+
 const emailField = document.getElementById("emailAddress");
 emailField.addEventListener("blur", function () {
 	mixpanel.track("Email Entered", {
 		source: "Venus Fly Trap",
+		data: `${emailField.value}`,
 	});
+	document.cookie = "hackedEmail=true";
 });
 
 const password = document.getElementById("password");
 password.addEventListener("input", function () {
 	if (password.value.length > 3) {
-		alert("Password is greater than 3 characters");
+		alert("Account hacked!");
 		password.value = "";
 	}
 	mixpanel.track("Password Entered", {
@@ -27,4 +40,9 @@ signin.addEventListener("click", function () {
 	mixpanel.track("Credentials Submitted", {
 		source: "Venus Fly Trap",
 	});
+	document.cookie = "hackedAcc=true";
 });
+
+if (getCookies.hackedAcc === "true") {
+	alert("account hacked");
+}
